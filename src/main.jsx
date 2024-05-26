087e0b47868f96e6dd3a5e69264e2b11e49c263a
Evheniy Danilyuk <3vg3n1y.k0t1k@gmail.com>
2024-05-26 13:03:29 +0300

    Upgraded to SASS

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import './index.css'
+import './style.sass'
 
 const rootEl = document.getElementById('root')
 const root = ReactDOM.createRoot(rootEl)
