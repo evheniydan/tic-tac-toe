6211c807043609f278396e2e768e02ffae8b71f2
Evheniy <3vg3n1y.k0t1k@gmail.com>
2024-10-27 15:59:02 +0200

    fixed base, updated packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tic-tac-toe/',
-  plugins: [react()],
-  server: {
-    open: true,
-    port: 8000,
-  },
+	plugins: [react()],
+	server: {
+		open: true,
+		port: 8000,
+	},
 })
